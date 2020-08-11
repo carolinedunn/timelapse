7abf02f30069085385128c994411947b3a8a36c7
Caroline Dunn <thecarolinedunn@gmail.com>
2020-08-11 19:35:35 -0400

    Update timelapse.py

--- timelapse.py
+++ timelapse.py
@@ -1,7 +1,7 @@
 from picamera import PiCamera
 from os import system
 import datetime
-import time
+from time import sleep
 
 tlminutes = 10 #set this to the number of minutes you wish to run your timelapse camera
 secondsinterval = 1 #number of seconds delay between each photo taken
@@ -18,7 +18,7 @@ camera.resolution = (1024, 768)
 
 for i in range(numphotos):
     camera.capture('/home/pi/Pictures/image{0:06d}.jpg'.format(i))
-    time.sleep(secondsinterval)
+    sleep(secondsinterval)
 print("Done taking photos.")
 print("Please standby as your timelapse video is created.")
 
